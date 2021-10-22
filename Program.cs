9c9c7c8889047dedf97585177dc7231302683b73
Shigina17 <AShigina163@mail.ru>
2021-10-22 19:40:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Индивидуальное домашнее задание");
+
+        }
+    }
+}
